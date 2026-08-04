cbbf88e8d9e946170499d98b52b6a668c8cf4319
Pradeep Telagimath <pradeeptelagimath@gmail.com>
2026-08-04 17:29:29 +0530

    D3

---
+++ track/M02/Add two numbers.py
@@ -0,0 +1,12 @@
+#read numbers
+
+a = int(input())
+b = int(input())
+
+#calculate the sum
+
+sum = a + b
+
+#print the sum
+
+print(f"Sum:{sum}")
---
+++ track/M02/type_conversion.py
@@ -0,0 +1,17 @@
+age_text = "21"
+course_fee_text = "499.50"
+attempts = 3
+
+#convert the values here 
+age = int(age_text)
+course_fee_text = float(course_fee_text)
+attempts_text = str(attempts)
+
+#Display the converted values and their datatypes
+
+print(age)
+print(type(age))
+print(course_fee_text)
+print(type(course_fee_text))
+print(attempts_text)
+print(type(attempts_text))
