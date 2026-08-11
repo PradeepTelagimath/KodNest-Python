782b3f061fdb6a2bb1cea5b4836250c944b0489b
Pradeep Telagimath <pradeeptelagimath@gmail.com>
2026-08-11 17:01:48 +0530

    D8

---
+++ track/M02/observe a shared Default List.py
@@ -0,0 +1,13 @@
+def add_student(name, students=None):
+    if students is None:
+        students = []
+    students.append(name)
+    print(students)
+
+first_name = input()
+second_name = input()
+third_name = input()
+
+add_student(first_name)
+add_student(second_name)
+add_student(third_name)
